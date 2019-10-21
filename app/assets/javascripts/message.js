3c200a9b61b2b898b69e2465343f6e166b84ce33
mf-Q <loud_flamingo@hotmail.com>
2019-10-22 01:11:34 +0900

    add to html message and error stlye to message.js
    送信したらメッセージ欄にhtmlの形式で情報が反映される。
    送信後、フォーム内の文字が自動で消える。
    送信ボタンのdisabled仕様を解除するコードを追加。
    送信できない時のエラーメッセージ追加。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,18 @@
 $(function(){
+
+  function buildMessage(message){
+    let html = `<div class="message__upper-info__talker">
+                ${message.user_name}
+                </div>
+                <div class="message__upper-info__date">
+                ${message.created_at}
+                </div>
+                <p class="message__text">
+                ${message.content}
+                </p>`
+    return html;
+  }
+
  $('#new_message').on('submit', function(e){
   e.preventDefault();
    let formData = new FormData(this);
@@ -13,9 +27,14 @@ $(function(){
      contentType: false
    })
   .done(function(message){
-    console.log(message);  
+    let html = buildMessage(message);
+    $('.messages').append(html)
+    $('#message_content').val('')
+    $('#.submit-btn').removeAttr('disabled');
   })
   .fail(function(){
+    alert('エラー');
+    
 
   })
  })
